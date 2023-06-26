53413b6d679e781129fac7d615d999043ed96a64
Tony <mantony0148@icloud.com>
2023-06-25 22:58:47 -0500

    Updates

---
+++ sb-addons/storybook-9/manager-bundle.js
@@ -0,0 +1,4 @@
+try{
+var c=__STORYBOOKADDONS__,{addons:o,types:h,mockChannel:p}=__STORYBOOKADDONS__;var x=__STORYBOOKTHEMING__,{CacheProvider:f,ClassNames:S,Global:u,ThemeProvider:k,background:y,color:C,convert:N,create:t,createCache:R,createGlobal:v,createReset:B,css:D,darken:G,ensure:K,ignoreSsrWarning:L,isPropValid:Y,jsx:I,keyframes:P,lighten:A,styled:E,themes:H,typography:M,useTheme:j,withTheme:w}=__STORYBOOKTHEMING__;var s=t({id:"Labsix",base:"light",brandTitle:"Labsix react-hooks-vimeo",brandImage:"/assets/Labsix Logo + name.png",brandTarget:"_self"});o.setConfig({theme:s});
+}catch(e){ console.error("[Storybook] One of your manager-entries failed: " + import.meta.url, e); }
+//# sourceMappingURL=manager-bundle.js.map
